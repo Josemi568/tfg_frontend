b727c9dd500229de3aabe695c23c82b76e9c3932
Jose Miguel Garcia <josegarmal1811@gmail.com>
2026-04-10 19:55:20 +0200

    twentiethCommit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,22 +15,22 @@ const Footer = () => {
       <div className='footer-container'>
         {/* Lado izquierdo: Información */}
         <div className='footer-info'>
-          <p style={{ margin: '5px 0' }}>Aviso legal</p>
-          <p><a href="#" rel="noopener noreferrer" style={{ margin: '5px 0' }}>Politica de privacidad</a></p>
-          <p><a href="#" rel="noopener noreferrer" style={{ margin: '5px 0' }}>Politica de cookies</a></p>
+          <p>Aviso legal</p>
+          <p><a href="#" rel="noopener noreferrer">Politica de privacidad</a></p>
+          <p><a href="#" rel="noopener noreferrer">Politica de cookies</a></p>
         </div>
 
         <div className='footer-copyright'>
-          <p style={{ margin: '5px 0' }}>Copyright © 2026 Plazart. Todos los derechos reservados.</p>
+          <p>Copyright © 2026 Plazart. Todos los derechos reservados.</p>
         </div>
 
         {/* Lado derecho: Imágenes */}
         <div className='footer-icons'>
           <a href="#" rel="noopener noreferrer">
-            <img src="/Images/telefono.png" alt="Telefono" style={{ width: '40px', height: '40px', objectFit: 'cover', borderRadius: '8px', backgroundColor: '#ffffff' }} />
+            <img src="/Images/telefono.png" alt="Telefono" />
           </a>
           <a href="#" rel="noopener noreferrer">
-            <img src="/Images/correo.png" alt="Correo" style={{ width: '40px', height: '40px', objectFit: 'cover', borderRadius: '8px', backgroundColor: '#ffffff' }} />
+            <img src="/Images/correo.png" alt="Correo" />
           </a>
         </div>
       </div>
